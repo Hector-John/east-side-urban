af8065f2dbef92a49cf6acb8d5dfd6db7a0431d4
Hector-John <hectorjohn254@gmail.com>
2025-04-12 02:56:09 +0300

    renamed assets

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,53 @@
+import nikeLogo from "./nikeLogo.png";
+import timberlandLogo from "./timberlandLogo.png";
+import adidasLogo from "./adidasLogo.png";
+import pumaLogo from "./pumaLogo.png";
+import converseLogo from "./converseLogo.png";
+import nbLogo from "./nbLogo.png";
+import vansLogo from "./vansLogo.png";
+import clarksLogo from "./clarksLogo.png";
+import banner_kids from "./banner_kids.png";
+import banner_mens from "./banner_mens.png";
+import banner_women from "./banner_women.png";
+import header_img from "./header_img.jpg";
+import heroBg from "./heroBg.png";
+import logo from "./logo.png";
+import accesoriesHeader from "./accesoriesHeader.webp";
+import earing from "./earing.webP";
+import accesories from "./accesories.webp";
+import hero1 from "./hero1.webP";
+import hero2 from "./hero2.webP";
+import hero12 from "./hero12.png";
+import men1 from "./men1.webP";
+import profile_icon from './profile_icon.png'
+import cover_photo from './cover_photo.png'
+import account from './account.webp'
+
+const images = {
+  cover_photo,
+  profile_icon,
+  account,
+  earing,
+  accesoriesHeader,
+  nikeLogo,
+  timberlandLogo,
+  adidasLogo,
+  pumaLogo,
+  converseLogo,
+  nbLogo,
+  vansLogo,
+  clarksLogo,
+  banner_kids,
+  banner_mens,
+  banner_women,
+  header_img,
+  heroBg,
+  logo,
+  hero1,
+  hero2,
+  hero12,
+  men1,
+  accesories,
+};
+
+export default images;
